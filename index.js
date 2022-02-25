c02935dc137cb96fe11fc318cddb36a5672f41e8
Mehedi109 <you@example.com>
2022-02-25 18:50:24 +0600

    payment api integrated

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const { MongoClient } = require('mongodb');
-const ObjectId = require('mongodb').ObjectId;
-require('dotenv').config();
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const stripe = require("stripe").Stripe(process.env.STRIPE_SECRET);
+
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -19,57 +21,77 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const database = client.db('drone-store');
-    const productsCollection = database.collection('products');
-    const ordersCollection = database.collection('orders');
-    const usersCollection = database.collection('users');
-    const reviewsCollection = database.collection('reviews');
+    const database = client.db("drone-store");
+    const productsCollection = database.collection("products");
+    const ordersCollection = database.collection("orders");
+    const usersCollection = database.collection("users");
+    const reviewsCollection = database.collection("reviews");
 
     // get api for products
-    app.get('/drones', async (req, res) => {
+    app.get("/drones", async (req, res) => {
       const cursor = await productsCollection.find({}).toArray();
       res.send(cursor);
     });
     // get api of products for specific id
-    app.get('/drones/:id', async (req, res) => {
+    app.get("/drones/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const cursor = await productsCollection.findOne(query);
       res.send(cursor);
     });
     // post api of order
-    app.post('/orders', async (req, res) => {
+    app.post("/orders", async (req, res) => {
       const orders = req.body;
       const result = await ordersCollection.insertOne(orders);
       res.json(result);
     });
+    // update orders
+    app.put("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          payment: payment,
+        },
+      };
+      const result = await ordersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
     // get api for order
-    app.get('/orders', async (req, res) => {
+    app.get("/orders", async (req, res) => {
       const cursor = await ordersCollection.find({}).toArray();
       res.send(cursor);
     });
+    // get api of orders for specific id
+    app.get("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const cursor = await ordersCollection.findOne(query);
+      res.send(cursor);
+    });
     // delete api for order
-    app.delete('/orders/:id', async (req, res) => {
+    app.delete("/orders/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await ordersCollection.deleteOne(query);
       res.send(result);
     });
     // post api for products
-    app.post('/products', async (req, res) => {
+    app.post("/products", async (req, res) => {
       const products = req.body;
       const result = productsCollection.insertOne(products);
       res.json(result);
     });
     // delete api for products
-    app.delete('/products/:id', async (req, res) => {
+    app.delete("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await productsCollection.deleteOne(query);
       res.send(result);
     });
     // get api for orders of login user
-    app.get('/myOrders', async (req, res) => {
+    app.get("/myOrders", async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
       console.log(query);
@@ -79,42 +101,42 @@ async function run() {
       res.send(result);
     });
     // post api for users
-    app.post('/users', async (req, res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
       const cursor = await usersCollection.insertOne(user);
       res.send(cursor);
     });
-    app.get('/users/:email', async (req, res) => {
+    app.get("/users/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const user = await usersCollection.findOne(query);
       let isAdmin = false;
-      if (user?.role === 'admin') {
+      if (user?.role === "admin") {
         isAdmin = true;
       }
       res.json({ admin: isAdmin });
     });
-    app.put('/users/admin', async (req, res) => {
+    app.put("/users/admin", async (req, res) => {
       const user = req.body;
-      console.log('put', user);
+      console.log("put", user);
       const filter = { email: user.email };
-      const updateDoc = { $set: { role: 'admin' } };
+      const updateDoc = { $set: { role: "admin" } };
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.json(result);
     });
     // post api for review
-    app.post('/review', async (req, res) => {
+    app.post("/review", async (req, res) => {
       const review = req.body;
       const cursor = await reviewsCollection.insertOne(review);
       res.send(cursor);
     });
     // get api for review
-    app.get('/reviews', async (req, res) => {
+    app.get("/reviews", async (req, res) => {
       const cursor = await reviewsCollection.find({}).toArray();
       res.send(cursor);
     });
     // update status
-    app.put('/updateStatus/:id', (req, res) => {
+    app.put("/updateStatus/:id", (req, res) => {
       const id = req.params.id;
       const updatedStatus = req.body.status;
       const result = ordersCollection.find({});
@@ -128,6 +150,18 @@ async function run() {
           res.send(result);
         });
     });
+
+    // stripe payment method
+    app.post("/create-payment-intent", async (req, res) => {
+      const paymentInfo = req.body;
+      const amount = paymentInfo.price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.json({ clientSecret: paymentIntent.client_secret });
+    });
   } finally {
     // await client.close();
   }
@@ -135,8 +169,8 @@ async function run() {
 
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Running the server!');
+app.get("/", (req, res) => {
+  res.send("Running the server!");
 });
 
 app.listen(port, () => {
